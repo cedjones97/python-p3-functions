7ce4150a4b9783c25faa24a2feb0f2d7af97183d
cedric <cedricj765@gmail.com>
2023-11-29 15:44:59 -0600

    passed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
+
+greet('Naureen')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(1, 2)
 
 def halve(number):
-    pass
+    return number / 2
+
+halve(4)
